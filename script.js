3391d3f7ee6c929f1e7f84bca4584cb42a658130
MarGba <marleneguzmanbahena47@gmail.com>
2026-05-24 20:15:43 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function encenderFoco(){
+    document.getElementById("foco").src =
+    "https://images.icon-icons.com/2104/PNG/512/bulb_icon_129222.png";
+}
+
+function apagarFoco(){
+    document.getElementById("foco").src =
+    "https://images.icon-icons.com/3710/PNG/512/bulb_idea_energy_electricity_lamp_light_icon_230099.png";
+}
